5e0ca27e7af23cd62b52bc7e11c35b026dead9cc
bthebault35 <75309825+bthebault35@users.noreply.github.com>
2022-04-25 20:13:05 +0200

    Add files via upload

--- Projet.py
+++ Projet.py
@@ -7,8 +7,10 @@ import matplotlib.pyplot as plt
 import os
 from math import sqrt
 from bokeh.plotting import figure, show, output_file
-from bokeh.models import HoverTool,ColumnDataSource,Div, Column
+from bokeh.models import HoverTool,ColumnDataSource,Div, Column,Row
 from bokeh.tile_providers import get_provider, Vendors
+from bokeh.models.widgets import Tabs, Panel
+from bokeh.layouts import column, row
 import numpy as np
 import pandas as pd
 import datetime
@@ -18,7 +20,7 @@ curdoc().theme = 'light_minimal' #thème des graphique
 
 ###########################DEFINITION DE FONCTION############################################################
 
-
+#permet de passer les coordoonées au bon format
 def coor_wgs84_to_web_mercator(lon, lat):
     k = 6378137
     x = lon * (k * np.pi/180.0)
@@ -36,6 +38,9 @@ coll=db['dump_Jan2022']
 
 
 
+#######################################################################################
+#REQUETE noSQL
+##########################################################################################
 
 rennes={"type": "Point", "coordinates": [-1.665177375354294, 48.117037996514576]}
 date1=datetime.datetime.strptime("2022/01/26", "%Y/%m/%d")
@@ -43,29 +48,45 @@ date2=datetime.datetime.strptime("2022/01/30", "%Y/%m/%d")
 
 #première requete pour récupérer les centres qui ont des créneaux aux dates correspondantes et ne garder que ces créneaux
 creneau_entre_date=coll.find({"location":{"$near": {"$geometry": rennes,"$maxDistance": 50000}},
-                  "visit_motives.slots":{"$elemMatch":{"$gte":date1,"$lt":date2}}},{"name":True,"visit_motives":True,"location.coordinates":True})
+                  "visit_motives.slots":{"$gte":date1,"$lt":date2}},{"name":True,"visit_motives":True,"location.coordinates":True})
 
 #2e requete pour récupérer les centres qui n'ont pas de creneaux sur la période
 not_creneau=coll.find({"location":{"$near": {"$geometry": rennes,"$maxDistance": 50000}},
                   "$nor": [{"visit_motives.slots":{"$gte":date1,"$lt":date2}}]},{"name":True,"visit_motives":True,"location.coordinates":True})
 
 
+
+
+#############################################################################################################
+
+
+
+
+
+
+
+#############################################################################################################
+#1ere VISU
+#############################################################################################################
+
 coordx,coordy,name,creneau,color,legend=[],[],[],[],[],[]
 for line in creneau_entre_date:
     nb_creneau=0
-    for motives in line['visit_motives']:
-        nb_creneau+=len(motives['slots'])
+    for motives in line['visit_motives']: #On boucle pour compter le nombre de date qui correspondent aux attentes
+        for date in motives["slots"]:
+            if date>=date1 and date<date2:
+                nb_creneau+=1
     xy=coor_wgs84_to_web_mercator(line["location"]["coordinates"][0],line["location"]["coordinates"][1])
     coordx.append(xy[0])
     coordy.append(xy[1])
     name.append(line["name"])
     creneau.append(nb_creneau)
-    if nb_creneau > 500:
+    if nb_creneau > 100:
         color.append("green")
-        legend.append("Plus de 500")
+        legend.append("Plus de 100")
     else:
         color.append("orange")
-        legend.append("Entre 1 et 500")
+        legend.append("Entre 1 et 100")
 
 
 
@@ -74,12 +95,12 @@ for line in not_creneau:
     coordx.append(xy[0])
     coordy.append(xy[1])
     name.append(line["name"])
-    creneau.append(0)
+    creneau.append(0) #ici on n'a que des centres sans créneau correspondant, pas besoin de boucler
     color.append("red")
     legend.append("Aucun")
 
 
-
+print(creneau)
 #Création de la carte
 map = figure(x_axis_type="mercator", y_axis_type="mercator",active_scroll="wheel_zoom", title="Centres de vaccination autour de Rennes, créneaux du 26 au 29 Janvier 2022",toolbar_location=None,width=1000,height=800)
 tile_provider = get_provider(Vendors.CARTODBPOSITRON)
@@ -119,10 +140,110 @@ map.yaxis.minor_tick_line_color = None
 map.yaxis.axis_line_color = None
 map.xaxis.axis_line_color = None
 
+#############################################################################################################
 
 
-########################################################
-texte1=Div(text="""
+
+
+
+#############################################################################################################
+#2e visu
+#############################################################################################################
+date3=datetime.datetime.strptime("2022/01/01", "%Y/%m/%d")
+date4=datetime.datetime.strptime("2022/06/01", "%Y/%m/%d")
+
+creneau_entre_date=coll.find({"location":{"$near": {"$geometry": rennes,"$maxDistance": 50000}},
+                  "visit_motives.slots":{"$gte":date3,"$lt":date4}},{"name":True,"visit_motives":True,"location.coordinates":True})
+
+#2e requete pour récupérer les centres qui n'ont pas de creneaux sur la période
+not_creneau=coll.find({"location":{"$near": {"$geometry": rennes,"$maxDistance": 50000}},
+                  "$nor": [{"visit_motives.slots":{"$gte":date3,"$lt":date4}}]},{"name":True,"visit_motives":True,"location.coordinates":True})
+
+coordx,coordy,name,creneau,color,legend=[],[],[],[],[],[]
+for line in creneau_entre_date:
+    nb_creneau=0
+    for motives in line['visit_motives']: #On boucle pour compter le nombre de date qui correspondent aux attentes
+        if motives["first_shot_motive"]==True:
+            for date in motives["slots"]:
+                if date>=date3 and date<date4:
+                    nb_creneau+=1
+
+    xy=coor_wgs84_to_web_mercator(line["location"]["coordinates"][0],line["location"]["coordinates"][1])
+    coordx.append(xy[0])
+    coordy.append(xy[1])
+    name.append(line["name"])
+    creneau.append(nb_creneau)
+    if nb_creneau > 100:
+        color.append("green")
+        legend.append("Plus de 100")
+    else:
+        color.append("orange")
+        legend.append("Entre 1 et 100")
+
+
+
+
+for line in not_creneau:
+    xy=coor_wgs84_to_web_mercator(line["location"]["coordinates"][0],line["location"]["coordinates"][1])
+    coordx.append(xy[0])
+    coordy.append(xy[1])
+    name.append(line["name"])
+    creneau.append(0) #ici on n'a que des centres sans créneau correspondant, pas besoin de boucler
+    color.append("red")
+    legend.append("Aucun")
+
+#Création de la carte
+map2 = figure(x_axis_type="mercator", y_axis_type="mercator",active_scroll="wheel_zoom", title="Centres de vaccination autour de Rennes, créneaux pour 1ere injection (Janvier à Juin 2022)",toolbar_location=None,width=1000,height=800)
+tile_provider = get_provider(Vendors.CARTODBPOSITRON)
+map2.add_tile(tile_provider)
+df=pd.DataFrame({"name":name,"x":coordx,"y":coordy,"creneau":creneau,"color":color,"legende":legend})
+
+lst_label=np.unique(legend) #liste des 3 labels
+
+#Pour avoir une légende intéractive, on trace label par label
+for label in lst_label:
+    df_label=df[df["legende"]==label]
+    source=ColumnDataSource(df_label)
+    map2.circle("x","y",color='color',size=15,source=source,legend_group="legende")
+    hover_tool = HoverTool(tooltips=[('Centre', '@name'),('Nombre de crénaux', '@creneau')])
+    map2.add_tools(hover_tool)
+
+#Paramètres graphiques
+map2.legend.title = "Nombre de créneaux"
+map2.legend.label_text_font = "times"
+map2.legend.label_text_font_style = "bold"
+map2.legend.label_text_color = "black"
+map2.legend.border_line_width = 3
+map2.legend.border_line_color = "black"
+map2.legend.border_line_alpha = 1
+map2.legend.background_fill_color = "white"
+map2.legend.background_fill_alpha = 1
+map2.legend.click_policy="hide"
+map2.legend.location="top_right"
+map2.xgrid.grid_line_color = None
+map2.ygrid.grid_line_color = None
+map2.xaxis.major_label_text_color = None
+map2.yaxis.major_label_text_color = None
+map2.xaxis.major_tick_line_color = None
+map2.xaxis.minor_tick_line_color = None
+map2.yaxis.major_tick_line_color = None
+map2.yaxis.minor_tick_line_color = None
+map2.yaxis.axis_line_color = None
+map2.xaxis.axis_line_color = None
+
+
+################################################################################################################
+
+
+
+
+
+
+
+################################################################################################################
+#MISE EN PAGE
+################################################################################################################
+entete=Div(text="""
 <style>
 body { 
     background-color: #2A2B2C; 
@@ -154,11 +275,18 @@ color: #2A2B2C;
 
 <body>
 <font color="white" size="4" face="Bookman Old Style, Book Antiqua, Garamond">
-<h2> Carte des centres de vaccination</h3>
+<h2> Carte des centres de vaccination</h2>
 </br>
 </br>
 </font></body>""")
 
+texte1=Div(text="""
+<body>
+<font color="white" size="4" face="Bookman Old Style, Book Antiqua, Garamond">
+</br>
+</br>
+
+</font></body>""")
 
 
 texte2=Div(text="""
@@ -166,10 +294,22 @@ texte2=Div(text="""
 <font color="white" size="4" face="Bookman Old Style, Book Antiqua, Garamond">
 </br>
 </br>
+<h2> Carte des centres de vaccination, tout type de visites (du 26 au 29 janvier 2022)</h2>
 <p> Cette carte nous montre que sur les 25 centres de vaccinations situés à moins de 50km de Rennes, plus de la moitié n'ont effectué de vaccination entre le 26 et le 29 Janvier 2022</p>
 </font></body>""")
 
-layout=Column(texte1,map,texte2)
-show(layout)
+layout=(entete)
+layout2=column(map,texte2)
+layout3=map2
+
+#Onglets principaux
+tab1 = Panel(child=layout2, title="Tous types de visites")
+tab2 = Panel(child=layout3, title="Visites pour 1ere dose")
+tabs = Tabs(tabs = [tab1, tab2])
+
+#Page globale
+page=column(layout,tabs)
+
+show(page)
 
 
